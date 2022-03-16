f29e4b8a142a6854583312948017300bf8247ced
Clara Chick <50504825+KohinaTheCat@users.noreply.github.com>
2022-03-16 15:34:47 -0400

    init commit

---
+++ game.asm
@@ -0,0 +1,35 @@
+##################################################################### 
+# 
+# CSCB58 Winter 2022 Assembly Final Project 
+# University of Toronto, Scarborough 
+# 
+# Student: Clara Chick, 1005946764, chickcla, clara.chick@mail.utoronto.ca
+# 
+# Bitmap Display Configuration: 
+# - Unit width in pixels: 4 (update this as needed)  
+# - Unit height in pixels: 4 (update this as needed) 
+# - Display width in pixels: 256 (update this as needed) 
+# - Display height in pixels: 256 (update this as needed) 
+# - Base Address for Display: 0x10008000 ($gp) 
+# 
+# Which milestones have been reached in this submission? 
+# (See the assignment handout for descriptions of the milestones) 
+# - Milestone 3
+# 
+# Which approved features have been implemented for milestone 3? 
+# (See the assignment handout for the list of additional features) 
+# 1. (fill in the feature, if any) 
+# 2. (fill in the feature, if any) 
+# 3. (fill in the feature, if any) 
+# ... (add more if necessary) 
+# 
+# Link to video demonstration for final submission: 
+# - (insert YouTube / MyMedia / other URL here). Make sure we can view it! 
+# 
+# Are you OK with us sharing the video with people outside course staff? 
+# - yes, and please share this project github link as well! 
+# 
+# Any additional information that the TA needs to know: 
+# - (write here, if any) 
+# 
+##################################################################### 
