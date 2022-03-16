fdcfd7210deed164da0254e806c6688761b851d9
Clara Chick <50504825+KohinaTheCat@users.noreply.github.com>
2022-03-16 17:29:37 -0400

    feat: player movement

--- game.asm
+++ game.asm
@@ -33,3 +33,142 @@
 # - (write here, if any) 
 # 
 ##################################################################### 
+
+# Define my constants
+.eqv BASE_ADDRESS 0x10008000
+.eqv LEFT 97
+.eqv RIGHT 100
+.eqv UP 119
+.eqv DOWN 115
+.eqv P 112
+.eqv GREEN 0x00FF00
+.eqv BLACK 0x000000
+
+.data
+	PLAYER: .word 1284
+	nl: 	.word '\n'
+
+.text
+
+.globl main
+	
+main:
+	li $t0, BASE_ADDRESS
+	li $t1, BLACK
+	li $t4, GREEN
+	
+	# Draw inital player
+	sw $t4,	1284($t0)
+	
+	j loop
+	
+	# Terminate the program gracefully, but should never reach this
+	li $v0, 10
+	syscall
+	
+loop:
+	# Check for  keypress
+	li $t9, 0xffff0000  
+	lw $t8, 0($t9) 
+	beq $t8, 1, keypress_happened  
+	
+	j loop
+
+keypress_happened:
+	# Check to see what key was pressed
+	lw $t2, 4($t9)
+	
+	# Check left
+	beq $t2, LEFT, on_left
+	# Check right
+	beq $t2, RIGHT, on_right
+	# Check up
+	beq $t2, UP, on_up
+	# Check down
+	beq $t2, DOWN, on_down
+	# Check p
+	beq $t2, P, on_p
+	
+	j loop
+	
+on_left:
+	# Get location of player
+	la $t5, PLAYER
+	
+	# Read location of player, let $a1 store the offset
+	lw $a1, 0($t5)
+	
+	# Colour old location as black, let $t3 the address to draw on
+	add $t3, $a1, $t0
+	sw $t1, 0($t3)
+
+	# Redraw player in new location
+	addi $a1, $a1, -4 # Get new offset
+	add $t3, $a1, $t0 # Get new address
+	sw $a1, 0($t5) # Save new location to array
+	sw $t4,	0($t3)# Draw player
+	
+	j loop
+
+on_right:
+	# Get location of player
+	la $t5, PLAYER
+	
+	# Read location of player, let $a1 store the offset
+	lw $a1, 0($t5)
+	
+	# Colour old location as black, let $t3 the address to draw on
+	add $t3, $a1, $t0
+	sw $t1, 0($t3)
+
+	# Redraw player in new location
+	addi $a1, $a1, 4 # Get new offset
+	add $t3, $a1, $t0 # Get new address
+	sw $a1, 0($t5) # Save new location to array
+	sw $t4,	0($t3)# Draw player
+	
+	j loop
+
+on_up:
+	# Get location of player
+	la $t5, PLAYER
+	
+	# Read location of player, let $a1 store the offset
+	lw $a1, 0($t5)
+	
+	# Colour old location as black, let $t3 the address to draw on
+	add $t3, $a1, $t0
+	sw $t1, 0($t3)
+
+	# Redraw player in new location
+	addi $a1, $a1, -128 # Get new offset
+	add $t3, $a1, $t0 # Get new address
+	sw $a1, 0($t5) # Save new location to array
+	sw $t4,	0($t3)# Draw player
+	
+	j loop
+
+on_down:
+	# Get location of player
+	la $t5, PLAYER
+	
+	# Read location of player, let $a1 store the offset
+	lw $a1, 0($t5)
+	
+	# Colour old location as black, let $t3 the address to draw on
+	add $t3, $a1, $t0
+	sw $t1, 0($t3)
+
+	# Redraw player in new location
+	addi $a1, $a1, 128 # Get new offset
+	add $t3, $a1, $t0 # Get new address
+	sw $a1, 0($t5) # Save new location to array
+	sw $t4,	0($t3)# Draw player
+	
+	j loop
+
+on_p:
+	j loop
+	
+	
+	
